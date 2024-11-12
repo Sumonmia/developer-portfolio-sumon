f212fb744d3ebad3063ad8646296bcfae1e2dab6
Sumonmia <sumon.ruetbd@gmail.com>
2024-11-12 19:26:24 +0600

    finalising the project

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -7,12 +7,12 @@ const AboutMe = () => {
     <div id='about' className="bg-blue-800 pt-32 pb-20">
       <div className='w-4/5 mx-auto flex flex-col lg:flex-row items-center justify-center'>
         <div className='w-1/3'>
-          <img src="sumon-1.png" alt="sumon" className='w-44'/>
+          <img src="sumonintro.png" alt="sumon" className='w-52'/>
         </div>
         <div className='w-2/3'>
-          <h1 className='text-white text-3xl'>Md.Sumon Mia</h1>
+          <h1 className='text-white text-3xl mb-5'>Md.Sumon Mia</h1>
           <AboutSlide />
-          <div className='flex space-x-7 mt-5'>
+          <div className='flex space-x-7 mt-10'>
             <a href="sumon-resume.pdf" target='_blank'className='btn border-0 hover:bg-blue-600 hover:text-white' >Download CV</a>
             <a href="#contact" className='btn border-0 hover:bg-blue-600 hover:text-white'>Contact Me</a>
           </div>
--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -8,7 +8,7 @@ const Introduction = () => {
         <h1 className='text-black text-3xl uppercase'>About Me</h1>
         <div className='flex space-x-7 mt-5'>
             <p>
-            My name is Sumon Mia. I am a Freelance Full-Stack Web Developer, and I'm very passionate and dedicated to my work. Having 3+ years experience as a professional Web developer and 8+ years experience in diverse filed of Computer Science & Engineering, I have acquired the skills and knowledge necessary to make your project a success. I enjoy every step of the development process, from discussion and collaboration to concept and execution, but I find the most satisfaction in seeing the finished product do everything for you that it was created to do.
+            My name is Sumon Mia. I am a Freelance Full-Stack Web Developer, and I'm very passionate and dedicated to my work. Having 3+ years experience as a professional Web developer and 8+ years experience in diverse field of Computer Science & Engineering, I have acquired the skills and knowledge necessary to make your project a success. I enjoy every step of the development process, from discussion and collaboration to concept and execution, but I find the most satisfaction in seeing the finished product do everything for you that it was created to do.
             </p>
         </div>
       </div>
